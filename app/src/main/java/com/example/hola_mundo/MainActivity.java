4ca86185ae740c6fc7c0b93206c5f94ce0b2f345
Carlos H García <carloshgcastrillon@gmail.com>
2021-10-06 11:09:28 -0500

    Añadiendo cambios al main activity xml y archivo java

--- app/src/main/java/com/example/hola_mundo/MainActivity.java
+++ app/src/main/java/com/example/hola_mundo/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.hola_mundo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtHola;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtHola=findViewById(R.id.txtView);
+        txtHola.setText("Hola a todos");
     }
 }
